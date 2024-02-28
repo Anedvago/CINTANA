817c7d24445a975f8432a0078e42d16b938f5e73
Anedvago <61481830+Anedvago@users.noreply.github.com>
2024-02-28 10:17:07 -0500

    paso previo a ng 17

--- src/app/modules/sales/components/form-head/form-head.component.ts
+++ src/app/modules/sales/components/form-head/form-head.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-form-head',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './form-head.component.html',
   styleUrls: ['./form-head.component.css']
 })
 export class FormHeadComponent {
 
+  formasDePago: string[] = ["1", "2"]
+  opcionesVentaPara: string[] = ["3", "4"]
 }
