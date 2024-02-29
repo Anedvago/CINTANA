0f4f733f0fb8a954465e482986c4aef868ee422d
Anedvago <61481830+Anedvago@users.noreply.github.com>
2024-02-29 14:33:11 -0500

    trabajando en el formulario de punto de venta

--- src/app/modules/sales/components/form-head/form-head.component.ts
+++ src/app/modules/sales/components/form-head/form-head.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-form-head',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [CommonModule, FormsModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './form-head.component.html',
   styleUrls: ['./form-head.component.css']
 })
-export class FormHeadComponent {
+export class FormHeadComponent implements OnInit {
 
-  formasDePago: string[] = ["EFECTIVO", "TRANSFERENCIA", "DATAFONO", "CHECK-OUT"]
+  opcionesFormasDePago: string[] = ["EFECTIVO", "TRANSFERENCIA", "DATAFONO", "CHECK-OUT"]
   opcionesVentaPara: string[] = ["CLIENTE", "HABITACION"]
+  ventaPara: string = "";
+  habitaciones: any[] = [];
+  constructor(private habitacionService: RoomService) {
+
+  }
+  ngOnInit(): void {
+
+    this.habitacionService.getAllRooms().then((data) => {
+      this.habitaciones = data
+
+    })
+  }
 
 }
