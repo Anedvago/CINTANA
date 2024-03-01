616cb2c647f08d2e5b6788642e6ea1c2eea9d5a1
Anedvago <61481830+Anedvago@users.noreply.github.com>
2024-03-01 11:43:46 -0500

    Cambios en el formulario

--- src/app/modules/sales/components/form-head/form-head.component.ts
+++ src/app/modules/sales/components/form-head/form-head.component.ts
@@ -7,6 +7,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
 import { RoomService } from 'src/app/services/room.service';
+import { ArticleService } from 'src/app/services/article.service';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-form-head',
@@ -21,15 +23,38 @@ export class FormHeadComponent implements OnInit {
   opcionesVentaPara: string[] = ["CLIENTE", "HABITACION"]
   ventaPara: string = "";
   habitaciones: any[] = [];
-  constructor(private habitacionService: RoomService) {
+  referencia: string = ""
+  listaDeArticulos: any[] = [];
+
+  constructor(private habitacionService: RoomService, private articuloService: ArticleService, private alertaService: AlertService) {
 
   }
   ngOnInit(): void {
 
-    this.habitacionService.getAllRooms().then((data) => {
-      this.habitaciones = data
-
-    })
+    this.obtenerTodasLasHabitaciones()
   }
 
+  obtenerTodasLasHabitaciones() {
+    this.habitacionService.getAllRooms().then((data) => {
+      this.habitaciones = data
+    })
+  }
+  buscarArticuloPorReferencia() {
+    this.articuloService.getArticleByRef(this.referencia).then((data: any) => {
+      if (data.length > 0) {
+        this.listaDeArticulos.push(data[0]);
+        console.log(this.listaDeArticulos);
+        //AQUI CAMBIAR Y COLOCAR UN OUTPUT CADA QUE SE BUSQUE UN ARTICULO
+        //EN EL COMPONENTE PADRE QUE SE VAYA GUARDANDO LA LISTA DE ARTICULOS
+        //QUE EL COMPONENTE PADRE LE ENVIE LA LISTA AL OTRO COMPONENTE QUE LISTA LOS ARTICULOS
+      } else {
+        this.alertaService.simpleAlert(
+          'error',
+          'Articulo No Encontrado',
+          'Digite el codigo nuevamente...'
+        );
+      }
+      this.referencia = "";
+    })
+  }
 }
